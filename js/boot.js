a1ab9f0bc4b5142646a26d3b39796b1e92c6bf11
zhangpenglin <88379382@qq.com>
2015-08-15 17:06:15 +0800

    new

---
+++ js/boot.js
@@ -0,0 +1,17 @@
+/*Boot Javascript*/
+;(function (window, $) {
+    window.PREFIX_URL = "http://localhost/";
+    window.PREFIX_S1_URL = "http://localhost/";
+    window.PREFIX_S2_URL = "http://localhost/";
+    window.PREFIX_HOST = "http://localhost";
+    window.PREFIX_HOST1 = "http://localhost";
+    window.PREFIX_FILE_HOST = "http://localhost";
+    window.CLIENT_CDN = "http://localhost/";
+    window.clientWidth = document.documentElement.clientWidth;
+    window.clientHeight = document.documentElement.clientHeight;
+
+    
+        document.write('<script src="js/js.js"></script>');
+    
+
+})(window, jQuery);
